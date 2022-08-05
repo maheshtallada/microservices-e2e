40d5e6c63646edc028a6e83a545c9c061aae89bb
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-08-05 12:48:47 +0530

    Added database and inserted few records

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into currency_exchange values (10001, 65, '', 'USD', 'INR');
+insert into currency_exchange values (10002, 85, '', 'EUR', 'INR');
+insert into currency_exchange values (10003, 45, '', 'AUD', 'INR');
