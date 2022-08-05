c5e9fbb3550d07fa5eaa4139ba3956fd13c365ee
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-08-05 12:15:51 +0530

    code to retrieve the currently running port and added help text to run same project in different ports

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchange.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -2,21 +2,37 @@ package com.learning.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
-import lombok.AllArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class CurrencyExchange {
 
-	private Long id;				
-	private String from;					
-	private String to;						
+	@Id
+	private Long id;
+	
+	@Column(name = "currencyFrom")
+	private String from;
+	
+	@Column(name = "currencyTo")
+	private String to;
+	
 	private BigDecimal conversionMultiple;
-	//private String environment;				
+	private String environment;
 
+	public CurrencyExchange(Long id, String from, String to, BigDecimal conversionMultiple) {
+		super();
+		this.id = id;
+		this.from = from;
+		this.to = to;
+		this.conversionMultiple = conversionMultiple;
+	}
 }
--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -2,6 +2,8 @@ package com.learning.microservices.currencyexchangeservice;
 
 import java.math.BigDecimal;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CurrencyExchangeController {
 
+	@Autowired
+	private Environment environment;
+
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangevalue(@PathVariable("from") String fromCurrency,
 			@PathVariable("to") String toCurrency) {
 
-		return new CurrencyExchange(1000L, fromCurrency, toCurrency, BigDecimal.valueOf(50));
+		String port = environment.getProperty("local.server.port");
+
+		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, fromCurrency, toCurrency,
+				BigDecimal.valueOf(50));
+
+		currencyExchange.setEnvironment(port);
+		return currencyExchange;
 
 	}
 
