a7783b027f928fec78011177e352e15eedcfa515
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-08-05 14:48:06 +0530

    Completed the currency-exchange-service logical flow

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,7 +1,5 @@
 package com.learning.microservices.currencyexchangeservice;
 
-import java.math.BigDecimal;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,17 +10,27 @@ import org.springframework.web.bind.annotation.RestController;
 public class CurrencyExchangeController {
 
 	@Autowired
+	private CurrencyExchangeRepository repo;
+	
+	@Autowired
 	private Environment environment;
 
 	@GetMapping("currency-exchange/from/{from}/to/{to}")
-	public CurrencyExchange retrieveExchangevalue(@PathVariable("from") String fromCurrency,
+	public CurrencyExchange retrieveExchangevalue(
+			@PathVariable("from") String fromCurrency,
 			@PathVariable("to") String toCurrency) {
 
 		String port = environment.getProperty("local.server.port");
 
-		CurrencyExchange currencyExchange = new CurrencyExchange(1000L, fromCurrency, toCurrency,
-				BigDecimal.valueOf(50));
+		CurrencyExchange currencyExchange = 
+//				new CurrencyExchange(1000L, fromCurrency, toCurrency,
+//				BigDecimal.valueOf(50));
+				repo.findByFromAndTo(fromCurrency, toCurrency);
 
+		if (currencyExchange == null) {
+			throw new RuntimeException
+				("Unable to find data for "+fromCurrency+" to "+toCurrency);
+		}
 		currencyExchange.setEnvironment(port);
 		return currencyExchange;
 
