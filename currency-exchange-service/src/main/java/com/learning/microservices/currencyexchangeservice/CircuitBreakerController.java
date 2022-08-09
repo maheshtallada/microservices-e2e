6327d3d521a7eb4a4517b25cd83c60934ba54c11
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-08-09 14:35:03 +0530

    Injecting error to work around the circuit breaker

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,13 +1,17 @@
 package com.learning.microservices.currencyexchangeservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 	
 	@GetMapping("/sample-api")
 	public String sampleApi() {
-		return "Sample API";
+
+		ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-api", String.class);
+		return forEntity.getBody();
 	}
 }
