106f693017f20f3574734b39264d0754e1fc4a6b
Mahesh Tallada <mahesh.tallada@gmail.com>
2022-08-24 16:04:35 +0530

    Added annotation and properties for rate limiting and controlling bulk head

--- currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/learning/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -7,8 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
@@ -19,6 +20,8 @@ public class CircuitBreakerController {
 	@GetMapping("/sample-api")
 	//@Retry(name = "sample-api", fallbackMethod="hardCodedResponse")
 	@CircuitBreaker(name = "default", fallbackMethod="hardCodedResponse")
+	@RateLimiter(name="default")
+	@Bulkhead(name="default")
 	public String sampleApi() {
 
 		logger.info("recieved sample api request");
